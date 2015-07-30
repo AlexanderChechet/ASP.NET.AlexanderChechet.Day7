b0b88e73111f069bc38d352e0dda4561fde66c06
AlexanderChechet <Chechetsasha95@gmail.com>
2015-07-30 13:12:03 +0300

    Day7Commit4

---
+++ Task4.Test/Task4Test.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task4.Test
+{
+    [TestClass]
+    public class Task4Test
+    {
+        [TestMethod]
+        public void BinarySearchFirst()
+        {
+            int[] array = {1, 3, 7, 11, 14, 15, 18};
+            
+            int result = BinarySearch<int>.Find(array, 1);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void BinarySearchLast()
+        {
+            int[] array = { 1, 3, 7, 11, 14, 15, 18 };
+
+            int result = BinarySearch<int>.Find(array, 18);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void BinarySearchMiddle()
+        {
+            int[] array = { 1, 3, 7, 11, 14, 15, 18 };
+
+            int result = BinarySearch<int>.Find(array, 11);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void BinarySearchUsual()
+        {
+            int[] array = { 1, 3, 7, 11, 14, 15, 18 };
+
+            int result = BinarySearch<int>.Find(array, 15);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void BinarySearchNoElement()
+        {
+            int[] array = { 1, 3, 7, 11, 14, 15, 18 };
+
+            int result = BinarySearch<int>.Find(array, 2);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void BinarySearchNoComparer()
+        {
+            Point[] array = { new Point(1, 1) };
+
+            BinarySearch<Point>.Find(array, new Point(1, 1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BinarySearchNoArray()
+        {
+            int[] array = null;
+
+            BinarySearch<int>.Find(array, 1);
+        }
+    }
+}
---
+++ Task4/BinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4
+{
+    public static class BinarySearch<T>
+    {
+        public static int Find(T[] array, T item)
+        {
+            int index = -1;
+            int left = 0;
+            if (array == null)
+                throw new NullReferenceException();
+            var comparable = item as IComparable<T>;
+            if (comparable == null)
+                throw new InvalidOperationException();
+            int right = array.Length - 1;
+            while (left <= right)
+            {
+                int middle = (left + right)/2;
+                int compareResult = comparable.CompareTo(array[middle]);
+                if (compareResult == 0)
+                {
+                    index = middle;
+                    break;
+                }
+                if (compareResult > 0)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            return index > 0 ? index : -((left + right) / 2 + 1);
+        }
+    }
+}
